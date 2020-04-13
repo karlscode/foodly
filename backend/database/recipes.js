2a2cb03e9f80d5796f22401f19c84491f9132130
Carlos Jesus <carlos.anderson.ti@gmail.com>
2020-04-13 19:27:44 -0300

    Add layout.njk

---
+++ backend/database/recipes.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: "1",
+        title: "Triplo bacon burger",
+        recipe: "",
+        by: "Jorge Relato",
+        featured: true
+    },
+    {
+        id: "2",
+        title: "Pizza 4 estações",
+        recipe: "",
+        by: "Fabiana Melo",
+        featured: true
+    },
+    {
+        id: "3",
+        title: "Espaguete ao alho",
+        recipe: "",
+        by: "Júlia Kinato",
+        featured: true
+    },
+    {
+        id: "4",
+        title: "Lasanha mac n' cheese",
+        recipe: "",
+        by: "Juliano Vieira",
+        featured: true
+    },
+    {
+        id: "5",
+        title: "Docinhos pão-do-céu",
+        recipe: "",
+        by: "Ricardo Golvea",
+        featured: true
+    },
+    {
+        id: "6",
+        title: "Asinhas de frango ao barbecue",
+        recipe: "",
+        by: "Vania Steroski",
+        featured: true
+    },
+]
